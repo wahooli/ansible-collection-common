018fc3bb00b09bc6b0092c4e67fa36ea2e92c39f
Waltteri Hooli <waltteri.hooli@gmail.com>
2024-03-06 00:45:45 +0200

    Add workflow dispatch and scheduled runs. Update tools/install-deps.sh to allow skipping installing either pip or ansible-galaxy collections

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 REPO_ROOT="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
+COLLECTION_INSTALL=${COLLECTION_INSTALL:-true}
+PIP_INSTALL=${PIP_INSTALL:-true}
 
-pip install -r $REPO_ROOT/requirements.txt
-ansible-galaxy install -r $REPO_ROOT/requirements.yml
+if [ "$PIP_INSTALL" = true ]; then
+    pip install -r $REPO_ROOT/requirements.txt
+fi
+if [ "$COLLECTION_INSTALL" = true ]; then
+    ansible-galaxy install -r $REPO_ROOT/requirements.yml
+fi
